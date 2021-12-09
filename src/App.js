7bfd68f98dc79a3eca6a56f7c9bf05c47549aa91
RTBlanco <ronnytoribio1@hotmail.com>
2021-12-08 20:51:24 -0500

    added riv images

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
 import './App.css';
+import {useRive, useStateMachineInput} from 'rive-react';
 
 function App() {
+
+  const { rive, RiveComponent} = useRive({
+    src: "switch.riv",
+    stateMachines: "State Machine 1",
+    // animations: "day",
+    autoplay: true,
+  })
+
+  const onClickInput = useStateMachineInput(rive, "State Machine 1", "Switch")
+
   return (
     <div>
-     
+      <RiveComponent onClick={() => onClickInput.fire()} className="switch"/>
+      {/* <RiveComponent /> */}
     </div>
   );
 }
+
+// function App() {
+
+//   const { rive, RiveComponent} = useRive({
+//     src: "hamburger-time.riv",
+//     stateMachines: "Basic State Machine",
+//     // animations: "day",
+//     autoplay: true,
+//   })
+
+//   const onClickInput = useStateMachineInput(rive, "Basic State Machine", "Switch")
+
+//   return (
+//     <div>
+//       <RiveComponent onClick={() => onClickInput.fire()}/>
+//       {/* <RiveComponent /> */}
+//     </div>
+//   );
+// }
 
 export default App;
